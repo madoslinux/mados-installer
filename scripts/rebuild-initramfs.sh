3c2d552b0225aab280f113bb35a4f171c0080fd0
madKoding <madkoding@gmail.com>
2026-03-26 18:13:31 +0000

    fix: always add essential storage modules (nvme, ahci, etc.) to initramfs

--- scripts/rebuild-initramfs.sh
+++ scripts/rebuild-initramfs.sh
@@ -37,12 +37,17 @@ EOFPRESET
 echo "  Detecting loaded kernel modules for initramfs..."
 MODULES=$(lsmod | awk 'NR>1 {print $1}' | tr '\n' ' ')
 MODULE_COUNT=$(lsmod | awk 'NR>1 {print $1}' | wc -w)
+STORAGE_MODULES="nvme ahci xhci_pci usb_storage virtio_scsi virtio_balloon sd_mod sr_mod cdrom ata_generic ata_piix ahci_platform"
 if [ -n "$MODULES" ]; then
-    echo "  Found ${MODULE_COUNT} modules to include in initramfs"
-    echo "  MODULES=\"$MODULES\"" >> /etc/mkinitcpio.conf
+    echo "  Found ${MODULE_COUNT} modules from lsmod"
+    echo "  Adding essential storage modules: ${STORAGE_MODULES}"
+    ALL_MODULES="${MODULES} ${STORAGE_MODULES}"
+    echo "MODULES=\"${ALL_MODULES}\"" >> /etc/mkinitcpio.conf
     echo "  Added modules to /etc/mkinitcpio.conf"
 else
     echo "  WARNING: No modules detected from lsmod"
+    echo "  Adding storage modules only: ${STORAGE_MODULES}"
+    echo "MODULES=\"${STORAGE_MODULES}\"" >> /etc/mkinitcpio.conf
 fi
 
 echo "  Current /etc/mkinitcpio.conf content:"
